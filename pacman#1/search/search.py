addd1cda25900eb7a154a76111c6fa3c19cb4905
GustavoLR548 <gustavolr035@gmail.com>
2022-06-24 22:46:54 -0300

    feat: adicionado exercicios faltantes

--- pacman#1/search/search.py
+++ pacman#1/search/search.py
@@ -1,80 +1,11 @@
-# search.py
-# ---------
-# Licensing Information:  You are free to use or extend these projects for
-# educational purposes provided that (1) you do not distribute or publish
-# solutions, (2) you retain this notice, and (3) you provide clear
-# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
-# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
-# The core projects and autograders were primarily created by John DeNero
-# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
-# Student side autograding was added by Brad Miller, Nick Hay, and
-# Pieter Abbeel (pabbeel@cs.berkeley.edu).
 
 POSITION  = 0
 DIRECTION = 1
 COST      = 2
 
-"""
-In search.py, you will implement generic search algorithms which are called by
-Pacman agents (in searchAgents.py).
-"""
-
 import queue
 import util
 
-class SearchProblem:
-    """
-    This class outlines the structure of a search problem, but doesn't implement
-    any of the methods (in object-oriented terminology: an abstract class).
-
-    You do not need to change anything in this class, ever.
-    """
-
-    def getStartState(self):
-        """
-        Returns the start state for the search problem.
-        """
-        util.raiseNotDefined()
-
-    def isGoalState(self, state):
-        """
-          state: Search state
-
-        Returns True if and only if the state is a valid goal state.
-        """
-        util.raiseNotDefined()
-
-    def getSuccessors(self, state):
-        """
-          state: Search state
-
-        For a given state, this should return a list of triples, (successor,
-        action, stepCost), where 'successor' is a successor to the current
-        state, 'action' is the action required to get there, and 'stepCost' is
-        the incremental cost of expanding to that successor.
-        """
-        util.raiseNotDefined()
-
-    def getCostOfActions(self, actions):
-        """
-         actions: A list of actions to take
-
-        This method returns the total cost of a particular sequence of actions.
-        The sequence must be composed of legal moves.
-        """
-        util.raiseNotDefined()
-
-
-def tinyMazeSearch(problem):
-    """
-    Returns a sequence of moves that solves tinyMaze.  For any other maze, the
-    sequence of moves will be incorrect, so only use this for tinyMaze.
-    """
-    from game import Directions
-    s = Directions.SOUTH
-    w = Directions.WEST
-    return  [s, s, w, s, w, w, s, w]    
     
 def depthFirstSearch(problem: SearchProblem):
 
@@ -84,9 +15,6 @@ def depthFirstSearch(problem: SearchProblem):
 
     start_node = problem.getStartState()
 
-    # A pilha será responsável em armazenar os pontos do labirinto, e 
-    # ao lado estará uma lista contendo as direções que o algoritmo precisa
-    # tomar para chegar ao caminho
     path = []
     path_stack.push((start_node, path))
 
@@ -124,9 +52,6 @@ def breadthFirstSearch(problem: SearchProblem):
 
     start_node = problem.getStartState()
 
-    # A fila será responsável em armazenar os pontos do labirinto, e 
-    # ao lado estará uma lista contendo as direções que o algoritmo precisa
-    # tomar para chegar ao caminho
     path = []
     path_stack.push((start_node, path))
 
@@ -161,7 +86,6 @@ def uniformCostSearch(problem: SearchProblem):
     priority_queue    = util.PriorityQueue()
     path              = []
 
-    # Guardar os caminhos já visitados
     visited = set() 
 
     start_node = problem.getStartState()
@@ -169,9 +93,6 @@ def uniformCostSearch(problem: SearchProblem):
     if problem.isGoalState(start_node):
         return ["Stop"]
         
-    # A fila será responsável em armazenar os pontos do labirinto, e 
-    # ao lado estará uma lista contendo as direções que o algoritmo precisa
-    # tomar para chegar ao caminho
     priority_queue.push((start_node,path), 0)
 
     while not priority_queue.isEmpty():
@@ -195,10 +116,6 @@ def uniformCostSearch(problem: SearchProblem):
 
 
 def nullHeuristic(state, problem=None):
-    """
-    A heuristic function estimates the cost from the current state to the nearest
-    goal in the provided SearchProblem.  This heuristic is trivial.
-    """
     return 0
 
 def aStarSearch(problem: SearchProblem, heuristic=nullHeuristic):
@@ -206,8 +123,6 @@ def aStarSearch(problem: SearchProblem, heuristic=nullHeuristic):
     priority_queue    = util.PriorityQueue()
     path              = []
 
-    # Dicionario guardando os pontos do grafo
-    # e o custo deles partindo da origem
     visited = set()
 
     start_node = problem.getStartState()
@@ -215,9 +130,6 @@ def aStarSearch(problem: SearchProblem, heuristic=nullHeuristic):
     if problem.isGoalState(start_node):
         return ["Stop"]
 
-    # A fila de prioridade será responsável em armazenar os pontos do labirinto, e 
-    # ao lado estará uma lista contendo as direções que o algoritmo precisa
-    # tomar para chegar ao caminho
     priority_queue.push((start_node,path), 0)
 
     while not priority_queue.isEmpty():
@@ -234,14 +146,11 @@ def aStarSearch(problem: SearchProblem, heuristic=nullHeuristic):
 
             if node[POSITION] not in visited:
 
-                # Mesmo cálculo do UCS, porém com heurística
                 cost=problem.getCostOfActions(path + [node[DIRECTION]]) + heuristic(node[POSITION],problem)
                 
                 priority_queue.push((node[POSITION], path + [node[DIRECTION]]), cost)
                 visited.add(node[POSITION])
 
-
-# Abbreviations
 bfs = breadthFirstSearch
 dfs = depthFirstSearch
 astar = aStarSearch
--- pacman#2/multiAgents.py
+++ pacman#2/multiAgents.py
@@ -1,75 +1,20 @@
-# multiAgents.py
-# --------------
-# Licensing Information:  You are free to use or extend these projects for
-# educational purposes provided that (1) you do not distribute or publish
-# solutions, (2) you retain this notice, and (3) you provide clear
-# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
-# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
-# The core projects and autograders were primarily created by John DeNero
-# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
-# Student side autograding was added by Brad Miller, Nick Hay, and
-# Pieter Abbeel (pabbeel@cs.berkeley.edu).
-
-
-from util import manhattanDistance
-from game import Directions
-import random, util
-
-from game import Agent
-from pacman import GameState
-
 PACMAN = 0
 
 class ReflexAgent(Agent):
-    """
-    A reflex agent chooses an action at each choice point by examining
-    its alternatives via a state evaluation function.
-
-    The code below is provided as a guide.  You are welcome to change
-    it in any way you see fit, so long as you don't touch our method
-    headers.
-    """
-
 
     def getAction(self, gameState: GameState):
-        """
-        You do not need to change this method, but you're welcome to.
 
-        getAction chooses among the best options according to the evaluation function.
-
-        Just like in the previous project, getAction takes a GameState and returns
-        some Directions.X for some X in the set {NORTH, SOUTH, WEST, EAST, STOP}
-        """
-        # Collect legal moves and successor states
         legalMoves = gameState.getLegalActions()
 
-        # Choose one of the best actions
         scores = [self.evaluationFunction(gameState, action) for action in legalMoves]
         bestScore = max(scores)
         bestIndices = [index for index in range(len(scores)) if scores[index] == bestScore]
-        chosen_index = random.choice(bestIndices) # Pick randomly among the best
-
-        "Add more of your code here if you want to"
+        chosen_index = random.choice(bestIndices) 
 
         return legalMoves[chosen_index]
 
     def evaluationFunction(self, currentGameState: GameState, action):
-        """
-        Design a better evaluation function here.
 
-        The evaluation function takes in the current and proposed successor
-        GameStates (pacman.py) and returns a number, where higher numbers are better.
-
-        The code below extracts some useful information from the state, like the
-        remaining food (newFood) and Pacman position after moving (newPos).
-        newScaredTimes holds the number of moves that each ghost will remain
-        scared because of Pacman having eaten a power pellet.
-
-        Print out these variables to see what you're getting, then combine them
-        to create a masterful evaluation function.
-        """
-        # Useful information you can extract from a GameState (pacman.py)
         successorGameState = currentGameState.generatePacmanSuccessor(action)
         newPos = successorGameState.getPacmanPosition()
         newFood = successorGameState.getFood()
@@ -94,10 +39,9 @@ class ReflexAgent(Agent):
                 min_ghost_distance     = distanceToGhosts[np.argmin(distanceToGhosts)]
                 nearestGhostScaredTime = newScaredTimes[np.argmin(distanceToGhosts)]
 
-                # Pacman is near a ghost that isnt scared
                 if min_ghost_distance <= 1 and nearestGhostScaredTime == 0:
                     return [min_ghost_distance, float('-inf')]
-                # Pacman is near a ghost that is scared
+
                 elif min_ghost_distance <= 1 and nearestGhostScaredTime > 0:
                     return [min_ghost_distance, float('inf') ]
 
@@ -111,7 +55,7 @@ class ReflexAgent(Agent):
 
         value = successorGameState.getScore() - min_food_distance
         if nearest_scared_ghost > 0:
-            # Follow ghosts that are scared
+
             value -= min_ghost_distance
         else:
             value += min_ghost_distance
@@ -119,29 +63,10 @@ class ReflexAgent(Agent):
         return value
 
 def scoreEvaluationFunction(currentGameState: GameState):
-    """
-    This default evaluation function just returns the score of the state.
-    The score is the same one displayed in the Pacman GUI.
 
-    This evaluation function is meant for use with adversarial search agents
-    (not reflex agents).
-    """
     return currentGameState.getScore()
 
 class MultiAgentSearchAgent(Agent):
-    """
-    This class provides some common elements to all of your
-    multi-agent searchers.  Any methods defined here will be available
-    to the MinimaxPacmanAgent, AlphaBetaPacmanAgent & ExpectimaxPacmanAgent.
-
-    You *do not* need to make any changes here, but you can if you want to
-    add functionality to all your adversarial search agents.  Please do not
-    remove anything, however.
-
-    Note: this is an abstract class: one that should not be instantiated.  It's
-    only partially specified, and designed to be extended.  Agent (game.py)
-    is another abstract class.
-    """
 
     def __init__(self, evalFn = 'scoreEvaluationFunction', depth = '2'):
         self.evaluationFunction = util.lookup(evalFn, globals())
@@ -158,9 +83,6 @@ class MultiAgentSearchAgent(Agent):
         return possible_actions
 
 class MinimaxAgent(MultiAgentSearchAgent):
-    """
-    Your minimax agent (question 2)
-    """
 
     def getAction(self, gameState):
 
@@ -202,15 +124,9 @@ class MinimaxAgent(MultiAgentSearchAgent):
         return possible_actions[chosen_index]
 
 class AlphaBetaAgent(MultiAgentSearchAgent):
-    """
-    Your minimax agent with alpha-beta pruning (question 3)
-    """
 
     def getAction(self, gameState):
-        """
-        Returns the minimax action from the current gameState using self.depth
-        and self.evaluationFunction using alpha-beta pruning.
-        """
+
         def alphabeta(agent, depth, gameState, alpha, beta):
 
             def minimize(agent, depth, gameState, alpha, beta):
@@ -250,7 +166,6 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
             else: 
                 return maximize(agent,depth,gameState,alpha,beta)
 
-        # Code starts here #
         possible_actions = self.getLegalActionsWithoutStop(0, gameState)
         alpha = float('-inf')
         beta  = float('inf')
@@ -264,30 +179,4 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
 
         return possible_actions[chosen_index]
 
-class ExpectimaxAgent(MultiAgentSearchAgent):
-    """
-      Your expectimax agent (question 4)
-    """
-
-    def getAction(self, gameState: GameState):
-        """
-        Returns the expectimax action using self.depth and self.evaluationFunction
-
-        All ghosts should be modeled as choosing uniformly at random from their
-        legal moves.
-        """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
-
-def betterEvaluationFunction(currentGameState: GameState):
-    """
-    Your extreme ghost-hunting, pellet-nabbing, food-gobbling, unstoppable
-    evaluation function (question 5).
-
-    DESCRIPTION: <write something here so we know what you did>
-    """
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
-
-# Abbreviation
 better = betterEvaluationFunction
